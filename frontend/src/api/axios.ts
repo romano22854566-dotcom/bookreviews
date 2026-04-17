2a47065443ad263ad100db411dc106db707f118c
PeTkun_Industries <romano22854566@gmail.com>
2026-04-17 09:08:43 +0300

    COMMIT

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || '';
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
